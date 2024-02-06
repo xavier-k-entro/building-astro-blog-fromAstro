607d93193eb056f35f60dc3e10d02e2b036d627d
Xavier K. <59243836+xk2800@users.noreply.github.com>
2024-02-06 16:12:07 +0800

    Add an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://xk2800.github.io'
+  site: 'https://reliable-cassata-d79c7f.netlify.app'
 });
