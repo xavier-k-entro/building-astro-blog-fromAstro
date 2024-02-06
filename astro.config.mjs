33027800decfe009f7e520fa21c3579e398256fb
Xavier K. <59243836+xk2800@users.noreply.github.com>
2024-02-06 16:37:09 +0800

    Build your first Astro island - Add Preact to your Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://reliable-cassata-d79c7f.netlify.app'
+  site: 'https://reliable-cassata-d79c7f.netlify.app',
+  integrations: [preact()]
 });
