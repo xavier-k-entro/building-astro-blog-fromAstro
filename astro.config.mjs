93ce634bd6d8e72760a0ecac908e9ccef637b55f
Xavier K. <59243836+xk2800@users.noreply.github.com>
2024-02-06 11:39:06 +0800

    update config file for gh pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://xk2800.github.io'.
+  site: 'https://xk2800.github.io'
 });
