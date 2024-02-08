a3a1ba1028bdc78d5b58aad9555ac612d2c8d0f1
Xavier K. <59243836+xk2800@users.noreply.github.com>
2024-02-08 22:16:50 +0800

    Update RSS function

--- src/content/posts/post-5-test.md
+++ src/content/posts/post-5-test.md
@@ -3,6 +3,9 @@
 title: This is my test blog
 author: Astro Learner
 description: "This post will show up on its own!"
+image:
+  url: "https://docs.astro.build/default-og-image.png"
+  alt: "The word astro against an illustration of planets and stars."
 pubDate: 2022-08-08
 tags: ["astro", "successes"]
 ---
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rssfrom '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
+
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-gb</language>`,
   });
 }
